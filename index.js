b67b98ac5a5cce2c9ed770c9bfb749e3d989f09b
jitendra <jitendra93266@gmail.com>
2020-05-06 09:46:42 +0530

    Subscribe function

---
+++ index.js
@@ -0,0 +1,27 @@
+function createStore(){
+    // The store should have 4 parts
+    /*
+    1. The state
+    2. Get the state
+    3. Listen to changes on the state
+    4. Update the state
+    */
+
+    let state = {} //The state
+    let listners = []
+    const getState = () => state  
+    const subscribe = (listner)=> {
+        console.log("Subscribed")
+        listners.push(listner)
+    }
+    return {
+        getState, //2.get the state
+        subscribe
+    }
+}
+
+
+const store = createStore()
+store.subscribe(()=>{
+    console.log("SUbscribed")
+})
